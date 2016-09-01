368513afb65fd5f7aa39317c145fd1b24043c6ea
Konstantin Snegov <ksnegov@ozon.ru>
2016-09-01 15:41:37 +0300

    Lesson 1

---
+++ lesson 1/Runner/Runner/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Runner
+//
+//  Created by Konstantin Snegov on 01/09/16.
+//  Copyright © 2016 Konstantin Snegov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+}
+
