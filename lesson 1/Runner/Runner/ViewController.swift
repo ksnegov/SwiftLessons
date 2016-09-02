8d27817e2b2172cb5e955b416800810f67661562
Konstantin Snegov <ksnegov@ozon.ru>
2016-09-02 14:54:31 +0300

    Add timer

--- lesson 1/Runner/Runner/ViewController.swift
+++ lesson 1/Runner/Runner/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
+	@IBOutlet weak var displayLabel: UILabel!
+	@IBOutlet weak var fireButton: UIButton!
+	
+	var timer : NSTimer?
+	var interval : NSTimeInterval = 0.0
+	let tick = 0.05
+	
+	var timerIsRunning = false
+	
+	@IBAction func tapFireButton() {
+		if timerIsRunning {
+			stop()
+		} else {
+			start()
+		}
+		timerIsRunning = !timerIsRunning
 	}
 	
+	func start() {
+		fireButton.setTitle("Stop", forState: .Normal)
+		
+		timer = NSTimer.scheduledTimerWithTimeInterval(tick, target: self, selector: #selector(timerChanged), userInfo: nil, repeats: true)
+		timer?.fire()
+	}
+	
+	func stop() {
+		fireButton.setTitle("Start", forState: .Normal)
+		
+		timer?.invalidate()
+	}
+	
+	func timerChanged() {
+		
+		interval += tick
+		
+		let minutes = Int(interval) / 60
+		let seconds = Int(interval) - minutes * 60
+		let miliseconds = Int(interval % 1 * 100)
+		
+		displayLabel.text = String(format: "%02d:%02d.%.02d", minutes, seconds, miliseconds)
+	}
 }
-
